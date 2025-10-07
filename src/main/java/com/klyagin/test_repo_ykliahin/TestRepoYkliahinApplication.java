cc2f755bd5c15984a2a7eb9db442c22ed2361904
Yevhenii Kliahin <yevhenii.kliahin@gmail.com>
2025-10-07 11:41:11 +0200

    init commit

---
+++ src/main/java/com/klyagin/test_repo_ykliahin/TestRepoYkliahinApplication.java
@@ -0,0 +1,13 @@
+package com.klyagin.test_repo_ykliahin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepoYkliahinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRepoYkliahinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klyagin/test_repo_ykliahin/TestRepoYkliahinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klyagin.test_repo_ykliahin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepoYkliahinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
